9cf6ad19d2b39fc470590e1b055dec06ecffd836
Tom Broderick <tombroderick@gmail.com>
2018-11-13 16:37:14 -0700

    initial commit

---
+++ src/ToDo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="ToDo">
+      <h1 class="ToDo-Header">To Do List</h1>
+        <table class="ToDo-Table">
+          <ToDoItem v-for="todo in list"
+                    :todo="todo"
+                    @delete="onDeleteItem"
+                    :key="todo.id" />
+        </table>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ToDoItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="ToDoItem">
+        <tr>
+            <td>{{ todo.text }} </td>
+            <td> Edit </td>
+            <td> Delete </td>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'to-do-item',
+  props: ['todo'],
+  methods: {
+      deleteItem(todo) {
+          this.$emit('delete', todo)
+      }
+  }
+}
+</script>
+
+<style>
+.ToDoItem {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
